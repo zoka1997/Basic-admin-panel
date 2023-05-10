467d4dcdead648f377063c1ecdcdd3e5492212da
zoka1997 <zoran.djukic.dev@gmail.com>
2023-05-10 13:42:57 +0200

    first commit

---
+++ BlogTechMVC/Controllers/AdminTagsController.cs
@@ -0,0 +1,41 @@
+﻿using BlogTechMVC.Data;
+using BlogTechMVC.Models.Domain;
+using BlogTechMVC.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace BlogTechMVC.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly BlogDbContext blogDbContext;
+
+        public AdminTagsController(BlogDbContext blogDbContext)
+        {
+            this.blogDbContext = blogDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            // Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            blogDbContext.Tags.Add(tag);
+            blogDbContext.SaveChanges(); // whit this method we save data
+
+            return View("Add");
+        }
+    }
+}
---
+++ BlogTechMVC/Data/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BlogTechMVC.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogTechMVC.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
